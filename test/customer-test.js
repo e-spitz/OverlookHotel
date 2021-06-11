c216e4b646baeb3e58c46249f64dd974fe0d54f3
Erica <ericaspitz522@gmail.com>
2021-06-11 12:25:03 -0600

    Add describe and beforeEach to customer test

--- test/customer-test.js
+++ test/customer-test.js
@@ -1,7 +1,23 @@
 import chai from 'chai';
-import Customer from '../src/customer'
 import Hotel from '../src/hotel'
+import Customer from '../src/customer'
 import Booking from '../src/booking'
 import Room from '../src/room'
 import { testCustomers, testRooms, testBookings } from './test-data'
 const expect = chai.expect;
+
+describe('Customer', () => {
+  let customer1, customer2, booking1, booking2, allBookings, room1, room2, allRooms, hotel;
+
+  beforeEach(() => {
+    customer1 = new Customer(testCustomers[1])
+    customer2 = new Customer(testCustomers[2])
+    booking1 = new Booking(testBookings[0])
+    booking2 = new Booking(testBookings[2])
+    allBookings = [booking1, booking2]
+    room1 = new Room(testRooms[0])
+    room2 = new Room(testRooms[1])
+    allRooms = [room1, room2]
+    hotel = new Hotel(allBookings, allRooms)
+  })
+})
