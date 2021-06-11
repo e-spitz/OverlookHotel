fab88bd0119aecfe373e78a0aa50f165499ac97c
Erica <ericaspitz522@gmail.com>
2021-06-11 12:14:18 -0600

    Add hotel class, Add test files

---
+++ test/customer-test.js
@@ -0,0 +1,7 @@
+import chai from 'chai';
+import Customer from '../src/customer'
+import Hotel from '../src/hotel'
+import Booking from '../src/booking'
+import Room from '../src/room'
+import { testCustomers, testRooms, testBookings } from './test-data'
+const expect = chai.expect;
